f93f6aca57e9bf37a5363e50e3ad3b1a0301b9c1
Jose Castellanos <neuralboot19@gmail.com>
2018-04-09 01:44:56 -0500

    Adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Candidate.create! full_name: "Zebeck Adriano", image: "https://www.puntofape.com/wp-content/uploads/2012/04/beneficios-deportes-extremos-300x200.jpg"
+Candidate.create! full_name: "Stalin Escobar", image: "https://cdn.oem.com.mx/elsoldepuebla/2017/06/deportes-extremos-600x400.jpg"
+Candidate.create! full_name: "Henry Zurita", image: "https://static.vix.com/es/sites/default/files/styles/large/public/imj/nuestrorumbo/5/5%20destinos%20para%20hacer%20deportes%20extremos.jpg?itok=m7WnRJlE"
+Candidate.create! full_name: "Paty Amaya", image: "https://i.pinimg.com/originals/65/59/50/655950a96465035bf85d1b19f4028755.jpg"
+Candidate.create! full_name: "Pablo Santos", image: "http://ve.emedemujer.com/wp-content/uploads/sites/2/2016/04/Rapel-destacada.jpg"
+Candidate.create! full_name: "David Arias", image: "https://www.puntofape.com/wp-content/uploads/2014/08/puntofape_consejos-para-realizar-un-deporte-de-riesgo.jpg"
+Candidate.create! full_name: "Monika Tamayo", image: "http://www.banzaj.pl/pictures/aktualnosci/rozne/ludzie/ludzie_sa_niesamowici_5.jpg"
+Candidate.create! full_name: "Gabo Rojas", image: "http://oboi-dlja-stola.ru/file/17061/760x0/16:9/%D0%9B%D1%8B%D0%B6%D0%BD%D1%8B%D0%B9-%D1%81%D0%BB%D0%B0%D0%BB%D0%BE%D0%BC.jpg"
+Candidate.create! full_name: "Ana Gordillo", image: "http://ataguaextremosangil.com/wp-content/uploads/2017/11/deportes-extremos-en-san-gil-2.jpg"
+Candidate.create! full_name: "David Cabrera", image: "http://revista.pricetravel.com.mx/wp-content/uploads/xpecaste/2015/12/jpg/Aspen-2.jpg"
+Candidate.create! full_name: "Rafaela Ospina", image: "http://sadabombon.com/wp-content/uploads/post/deportes-extremos/sb5_top10.jpg"
+Candidate.create! full_name: "Henry Manosalvas", image: "https://thumbs.dreamstime.com/b/un-jinete-profesional-en-la-competencia-de-fmx-motocr%C3%B3s-del-estilo-libre-en-los-deportes-extremos-barcelona-de-lkxa-46730310.jpg"
